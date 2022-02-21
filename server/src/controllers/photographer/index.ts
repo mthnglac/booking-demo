e2d8c4b5660100c791874b12fc2d0d51a4a075c7
Metehan Gulac <metehanglc@protonmail.com>
2022-02-21 03:14:31 +0300

    error handling logic moved to controller from service

--- server/src/controllers/photographer/index.ts
+++ server/src/controllers/photographer/index.ts
@@ -6,18 +6,31 @@ import {
 import { photographerService } from "../../services/photographer";
 
 const getPhotographers = async (req: Request, res: Response): Promise<void> => {
-  const photographers: IPhotographer[] = await photographerService.get();
-  res.status(200).send(photographers);
+  try {
+    const photographers: IPhotographer[] = await photographerService.get();
+
+    if (photographers.length) {
+      res.status(200).send(photographers);
+    } else {
+      res.status(404).send({ message: "Couldn't find any photographer." });
+    }
+  } catch {
+    res.status(500).send({ error: "Problem at server."})
+  }
 };
 
 const getAvailablePhotographersByDuration = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const durationInMinutes = req.query.duration as unknown as number;
-  const availablePhotographersWithTimeSlot: IPhotographerBookingResponse[] =
-    await photographerService.getAvailablesByDuration(durationInMinutes);
-  res.status(200).send(availablePhotographersWithTimeSlot);
+  try {
+    const durationInMinutes = req.query.duration as unknown as number;
+    const availablePhotographersWithTimeSlot: IPhotographerBookingResponse[] =
+      await photographerService.getAvailablesByDuration(durationInMinutes);
+    res.status(200).send(availablePhotographersWithTimeSlot);
+  } catch {
+    res.status(500).send({ error: "Problem at server."})
+  }
 };
 
 const createPhotographer = async (
@@ -29,28 +42,36 @@ const createPhotographer = async (
     "name" | "availabilities" | "bookings"
   >;
 
-  const photographer: IPhotographer = await photographerService.create(
-    body.name,
-    body.availabilities,
-    body.bookings
-  );
+  try {
+    const photographer: IPhotographer = await photographerService.create(
+      body.name,
+      body.availabilities,
+      body.bookings
+    );
 
-  res.status(201).send({ message: "Photographer added.", photographer });
+    res.status(201).send({ message: "Photographer added.", photographer });
+  } catch {
+    res.status(500).send({ error: "Problem at server."})
+  }
 };
 
 const patchPhotographer = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const photographer: IPhotographer | null = await photographerService.patch(
-    req.params.id,
-    req.body
-  );
+  try {
+    const photographer: IPhotographer | null = await photographerService.patch(
+      req.params.id,
+      req.body
+    );
 
-  if (!photographer) {
-    res.status(404).send({ message: "Photographer not found." });
-  } else {
-    res.status(200).send({ message: "Photographer updated.", photographer });
+    if (!photographer) {
+      res.status(404).send({ message: "Photographer not found." });
+    } else {
+      res.status(200).send({ message: "Photographer updated.", photographer });
+    }
+  } catch {
+    res.status(500).send({ error: "Problem at server."})
   }
 };
 
@@ -58,14 +79,18 @@ const deletePhotographer = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  const photographer: IPhotographer | null = await photographerService.delete(
-    req.params.id
-  );
+  try {
+    const photographer: IPhotographer | null = await photographerService.delete(
+      req.params.id
+    );
 
-  if (!photographer) {
-    res.status(404).send({ message: "Photographer doesn't exist!." });
-  } else {
-    res.status(204).send();
+    if (!photographer) {
+      res.status(404).send({ message: "Photographer doesn't exist!." });
+    } else {
+      res.status(204).send();
+    }
+  } catch {
+    res.status(500).send({ error: "Problem at server."})
   }
 };
 
--- server/src/services/photographer/index.ts
+++ server/src/services/photographer/index.ts
@@ -9,12 +9,8 @@ import { dateDiffInMinutesBetweenTwoDate } from '../../helpers/date-differ'
 
 const photographerService = {
   get: async (): Promise<IPhotographer[]> => {
-    try {
-      const photographers: IPhotographer[] = await Photographer.find();
-      return photographers;
-    } catch (error) {
-      throw error;
-    }
+    const photographers: IPhotographer[] = await Photographer.find();
+    return photographers;
   },
 
   getAvailablesByDuration: async (
@@ -23,151 +19,147 @@ const photographerService = {
     const reservedPhotographersWithTimeSlot: IPhotographerBookingResponse[] =
       [];
 
-    try {
-      const availablePhotographers = await Photographer.find({
-        availabilities: {
-          $exists: true,
-          $type: "array",
-          $ne: [],
-        },
-      });
+    const availablePhotographers = await Photographer.find({
+      availabilities: {
+        $exists: true,
+        $type: "array",
+        $ne: [],
+      },
+    });
 
-      // pushing earliest acceptable availabilities to the response
-      availablePhotographers.map((availablePhotographer) => {
-        const acceptableAvailabilities: IAvailability[] = [];
+    // pushing earliest acceptable availabilities to the response
+    availablePhotographers.map((availablePhotographer) => {
+      const acceptableAvailabilities: IAvailability[] = [];
 
-        availablePhotographer.availabilities.map((availability) => {
-          const dateDiffForAvailability = dateDiffInMinutesBetweenTwoDate(
-            availability.starts,
-            availability.ends,
-          )
-          // if there is any time period >= than duration
-          // push it to the acceptableAvailabilities
-          if (dateDiffForAvailability >= durationInMinutes) {
-            acceptableAvailabilities.push({
-              starts: availability.starts,
-              ends: availability.ends,
-            });
-          }
-        });
-
-        // We can not start reducing with empty array
-        if (acceptableAvailabilities.length) {
-          reservedPhotographersWithTimeSlot.push({
-            photographer: {
-              id: availablePhotographer._id,
-              name: availablePhotographer.name,
-            },
+      availablePhotographer.availabilities.map((availability) => {
+        const dateDiffForAvailability = dateDiffInMinutesBetweenTwoDate(
+          availability.starts,
+          availability.ends,
+        )
+        // if there is any time period >= than duration
+        // push it to the acceptableAvailabilities
+        if (dateDiffForAvailability >= durationInMinutes) {
+          acceptableAvailabilities.push({
+            starts: availability.starts,
+            ends: availability.ends,
           });
-
-          // shaving acceptableAvailabilities with bookings
-          if (availablePhotographer.bookings.length) {
-            availablePhotographer.bookings.map((booking) => {
-              const startsOfBooking = new Date(booking.starts).getTime()
-              const endsOfBooking = new Date(booking.ends).getTime()
-
-              acceptableAvailabilities.map((acceptableAvailability, acceptableAvailabilityIndex) => {
-                const startsOfAcceptableAvailability = new Date(acceptableAvailability.starts).getTime()
-                const endsOfAcceptableAvailability = new Date(acceptableAvailability.ends).getTime()
-
-                // if booking covers all the availability, remove the availability from acceptableAvailabilities.
-                if (startsOfBooking === startsOfAcceptableAvailability && endsOfBooking === endsOfAcceptableAvailability) {
-                  acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
-                }
-
-                // if booking starts at the beginning of the availability
-                if (startsOfBooking === startsOfAcceptableAvailability && endsOfBooking !== endsOfAcceptableAvailability) {
-                  acceptableAvailability.starts = booking.ends
-
-                  // if rest of availability < than duration, just remove it.
-                  const dateDiff = dateDiffInMinutesBetweenTwoDate(
-                    acceptableAvailability.starts,
-                    acceptableAvailability.ends
-                  )
-                  if (dateDiff < durationInMinutes) {
-                    acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
-                  }
-                }
-
-                // if booking ends at the ending of the availability
-                if (startsOfBooking === startsOfAcceptableAvailability && endsOfBooking !== endsOfAcceptableAvailability) {
-                  acceptableAvailability.ends = booking.starts
-
-                  // if rest of availability < than duration, just remove it.
-                  const dateDiff = dateDiffInMinutesBetweenTwoDate(
-                    acceptableAvailability.starts,
-                    acceptableAvailability.ends
-                  )
-                  if (dateDiff < durationInMinutes) {
-                    acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
-                  }
-                }
-
-                // if booking is at the somewhere in the availability, split the availability
-                if (startsOfBooking > startsOfAcceptableAvailability && endsOfBooking < endsOfAcceptableAvailability) {
-                  const newAcceptableAvailability = { starts: acceptableAvailability.starts, ends: acceptableAvailability.ends }
-                  acceptableAvailability.ends = booking.starts
-                  newAcceptableAvailability.starts = booking.ends
-
-                  // if first availability < than duration, just remove it.
-                  const dateDiffForFirst = dateDiffInMinutesBetweenTwoDate(
-                    acceptableAvailability.starts,
-                    acceptableAvailability.ends
-                  )
-                  if (dateDiffForFirst < durationInMinutes) {
-                    acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
-                  }
-
-                  // if second availability < than duration, just remove it.
-                  const dateDiffForSecond = dateDiffInMinutesBetweenTwoDate(
-                    newAcceptableAvailability.starts,
-                    newAcceptableAvailability.ends
-                  )
-                  if (dateDiffForSecond < durationInMinutes) {
-                    acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
-                  } else {
-                    acceptableAvailabilities.push({
-                      starts: newAcceptableAvailability.starts,
-                      ends: newAcceptableAvailability.ends,
-                    })
-                  }
-                }
-              })
-            })
-          }
-
-          // find earliest time period inside the acceptableAvailabilities
-          const earlierAcceptableAvailability = acceptableAvailabilities.reduce((earlierAcceptableAvailability, item) =>
-            item.starts < earlierAcceptableAvailability.starts ? item : earlierAcceptableAvailability
-          );
-
-          // find photographer index inside the reservedPhotographersWithTimeSlot
-          const reservedPhotographerIndex =
-            reservedPhotographersWithTimeSlot.findIndex(
-              ({ photographer }) =>
-                photographer.name === availablePhotographer.name
-            );
-
-          // add acceptableAvailability to the reserved photographer
-          Object.assign(
-            reservedPhotographersWithTimeSlot[reservedPhotographerIndex],
-            {
-              timeSlot: {
-                starts: earlierAcceptableAvailability.starts,
-                ends: new Date(
-                  new Date(earlierAcceptableAvailability.starts).getTime() +
-                    durationInMinutes * 60000
-                ),
-              },
-            }
-          );
         }
       });
 
-      return reservedPhotographersWithTimeSlot;
-    } catch (error) {
-      throw error;
-    }
+      // We can not start reducing with empty array
+      if (acceptableAvailabilities.length) {
+        reservedPhotographersWithTimeSlot.push({
+          photographer: {
+            id: availablePhotographer._id,
+            name: availablePhotographer.name,
+          },
+        });
+
+        // shaving acceptableAvailabilities with bookings
+        if (availablePhotographer.bookings.length) {
+          availablePhotographer.bookings.map((booking) => {
+            const startsOfBooking = new Date(booking.starts).getTime()
+            const endsOfBooking = new Date(booking.ends).getTime()
+
+            acceptableAvailabilities.map((acceptableAvailability, acceptableAvailabilityIndex) => {
+              const startsOfAcceptableAvailability = new Date(acceptableAvailability.starts).getTime()
+              const endsOfAcceptableAvailability = new Date(acceptableAvailability.ends).getTime()
+
+              // if booking covers all the availability, remove the availability from acceptableAvailabilities.
+              if (startsOfBooking === startsOfAcceptableAvailability && endsOfBooking === endsOfAcceptableAvailability) {
+                acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
+              }
+
+              // if booking starts at the beginning of the availability
+              if (startsOfBooking === startsOfAcceptableAvailability && endsOfBooking !== endsOfAcceptableAvailability) {
+                acceptableAvailability.starts = booking.ends
+
+                // if rest of availability < than duration, just remove it.
+                const dateDiff = dateDiffInMinutesBetweenTwoDate(
+                  acceptableAvailability.starts,
+                  acceptableAvailability.ends
+                )
+                if (dateDiff < durationInMinutes) {
+                  acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
+                }
+              }
+
+              // if booking ends at the ending of the availability
+              if (startsOfBooking === startsOfAcceptableAvailability && endsOfBooking !== endsOfAcceptableAvailability) {
+                acceptableAvailability.ends = booking.starts
+
+                // if rest of availability < than duration, just remove it.
+                const dateDiff = dateDiffInMinutesBetweenTwoDate(
+                  acceptableAvailability.starts,
+                  acceptableAvailability.ends
+                )
+                if (dateDiff < durationInMinutes) {
+                  acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
+                }
+              }
+
+              // if booking is at the somewhere in the availability, split the availability
+              if (startsOfBooking > startsOfAcceptableAvailability && endsOfBooking < endsOfAcceptableAvailability) {
+                const newAcceptableAvailability = { starts: acceptableAvailability.starts, ends: acceptableAvailability.ends }
+                acceptableAvailability.ends = booking.starts
+                newAcceptableAvailability.starts = booking.ends
+
+                // if first availability < than duration, just remove it.
+                const dateDiffForFirst = dateDiffInMinutesBetweenTwoDate(
+                  acceptableAvailability.starts,
+                  acceptableAvailability.ends
+                )
+                if (dateDiffForFirst < durationInMinutes) {
+                  acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
+                }
+
+                // if second availability < than duration, just remove it.
+                const dateDiffForSecond = dateDiffInMinutesBetweenTwoDate(
+                  newAcceptableAvailability.starts,
+                  newAcceptableAvailability.ends
+                )
+                if (dateDiffForSecond < durationInMinutes) {
+                  acceptableAvailabilities.splice(acceptableAvailabilityIndex, 1)
+                } else {
+                  acceptableAvailabilities.push({
+                    starts: newAcceptableAvailability.starts,
+                    ends: newAcceptableAvailability.ends,
+                  })
+                }
+              }
+            })
+          })
+        }
+
+        // find earliest time period inside the acceptableAvailabilities
+        const earlierAcceptableAvailability = acceptableAvailabilities.reduce((earlierAcceptableAvailability, item) =>
+          item.starts < earlierAcceptableAvailability.starts ? item : earlierAcceptableAvailability
+        );
+
+        // find photographer index inside the reservedPhotographersWithTimeSlot
+        const reservedPhotographerIndex =
+        reservedPhotographersWithTimeSlot.findIndex(
+          ({ photographer }) =>
+            photographer.name === availablePhotographer.name
+        );
+
+        // add acceptableAvailability to the reserved photographer
+        Object.assign(
+          reservedPhotographersWithTimeSlot[reservedPhotographerIndex],
+          {
+            timeSlot: {
+              starts: earlierAcceptableAvailability.starts,
+              ends: new Date(
+                new Date(earlierAcceptableAvailability.starts).getTime() +
+                  durationInMinutes * 60000
+              ),
+            },
+          }
+        );
+      }
+    });
+
+    return reservedPhotographersWithTimeSlot;
   },
 
   create: async (
@@ -175,43 +167,31 @@ const photographerService = {
     availabilities: IAvailability[],
     bookings: IBooking[]
   ): Promise<IPhotographer> => {
-    try {
-      const photographer: IPhotographer = new Photographer({
-        name,
-        availabilities,
-        bookings,
-      });
-      await photographer.save();
+    const photographer: IPhotographer = new Photographer({
+      name,
+      availabilities,
+      bookings,
+    });
+    await photographer.save();
 
-      return photographer;
-    } catch (error) {
-      throw error;
-    }
+    return photographer;
   },
 
   patch: async (
     id: string,
     body: Pick<IPhotographer, "name" | "availabilities" | "bookings">
   ): Promise<IPhotographer | null> => {
-    try {
-      const photographer: IPhotographer | null =
-        await Photographer.findByIdAndUpdate({ _id: id }, body, { new: true });
+    const photographer: IPhotographer | null =
+      await Photographer.findByIdAndUpdate({ _id: id }, body, { new: true });
 
-      return photographer;
-    } catch (error) {
-      throw error;
-    }
+    return photographer;
   },
 
   delete: async (id: string): Promise<IPhotographer | null> => {
-    try {
-      const deletedPhotographer: IPhotographer | null =
-        await Photographer.findByIdAndDelete(id);
+    const deletedPhotographer: IPhotographer | null =
+      await Photographer.findByIdAndDelete(id);
 
-      return deletedPhotographer;
-    } catch (error) {
-      throw error;
-    }
+    return deletedPhotographer;
   },
 };
 
